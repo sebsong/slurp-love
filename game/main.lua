58f74a9fd19f76d6036635d51445fc09b39fedae
Sebastian Song <sebastian.song@pragma.gg>
2026-01-12 16:53:35 -0800

    Initial commit

---
+++ game/main.lua
@@ -0,0 +1,17 @@
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+function love.load()
+end
+
+function love.update()
+	if love.keyboard.isDown("escape") then
+		love.event.quit()
+	end
+end
+
+function love.draw()
+	local width, height = love.graphics.getDimensions()
+	love.graphics.print("slurp's up", width / 2, height / 2)
+end
