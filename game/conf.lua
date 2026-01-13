deb3dbb25c382aa9dbbf2834406390f2950258ae
Sebastian Song <sebastianbsong@gmail.com>
2026-01-12 19:15:16 -0800

    no fullscreen

--- game/conf.lua
+++ game/conf.lua
@@ -20,15 +20,15 @@ function love.conf(t)
 	t.audio.mic             = false
 	t.audio.mixwithsystem   = true
 
-	t.window.title          = "Untitled"
+	t.window.title          = "slurp's up!"
 	t.window.icon           = nil
-	t.window.width          = 800
-	t.window.height         = 600
+	t.window.width          = 1280
+	t.window.height         = 720
 	t.window.borderless     = false
 	t.window.resizable      = false
 	t.window.minwidth       = 1
 	t.window.minheight      = 1
-	t.window.fullscreen     = true
+	t.window.fullscreen     = false
 	t.window.fullscreentype = "desktop"
 	t.window.vsync          = 1
 	t.window.msaa           = 0
