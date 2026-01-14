3547f7e3d46ab80b06d9d7d55475e6b7bc981231
Sebastian Song <sebastianbsong@gmail.com>
2026-01-13 23:50:58 -0800

    add entities art

--- game/main.lua
+++ game/main.lua
@@ -1,13 +1,29 @@
 function love.load()
+	-- entitiesImage = love.graphics.newImage("assets/art/entities.png")
+	rectPos = { x = 100, y = 100 }
+	speed = 300
 end
 
-function love.update()
+function love.update(dt)
 	if love.keyboard.isDown("escape") then
 		love.event.quit()
+	end
+
+	if love.keyboard.isDown("up") then
+		rectPos.y = rectPos.y - speed * dt;
+	end
+	if love.keyboard.isDown("down") then
+		rectPos.y = rectPos.y + speed * dt;
+	end
+	if love.keyboard.isDown("left") then
+		rectPos.x = rectPos.x - speed * dt;
+	end
+	if love.keyboard.isDown("right") then
+		rectPos.x = rectPos.x + speed * dt;
 	end
 end
 
 function love.draw()
-	local width, height = love.graphics.getDimensions()
-	love.graphics.print("slurp's up", width / 2, height / 2)
+	-- love.graphics.draw(entitiesImage)
+	love.graphics.rectangle("fill", rectPos.x, rectPos.y, 50, 80)
 end
