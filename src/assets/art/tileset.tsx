28cab3db25a2804b1c9fbb5744b7b086aa7ab0fe
Sebastian Song <seby.song@gmail.com>
2026-01-22 18:39:56 -0800

    update tileset

--- src/assets/art/tileset.tsx
+++ src/assets/art/tileset.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="tileset.aseprite" width="16" height="16"/>
+ <grid orientation="isometric" width="16" height="16"/>
+ <image source="tileset.png" width="16" height="16"/>
 </tileset>
