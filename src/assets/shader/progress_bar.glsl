243b3716f271b16337a85af1b41a735ef5baa69e
Sebastian Song <seby.song@gmail.com>
2026-02-06 00:07:28 -0800

    add vertex shader in same file

--- src/assets/shader/progress_bar.glsl
+++ src/assets/shader/progress_bar.glsl
@@ -2,6 +2,13 @@
 
 uniform float progress;
 
+#ifdef VERTEX
+vec4 position( mat4 transform_projection, vec4 vertex_position ) {
+	return transform_projection * vertex_position;
+}
+#endif
+
+#ifdef PIXEL
 vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
 {
 	if (texture_coords.y < 1.0 - progress) {
@@ -11,3 +18,4 @@ vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords )
     vec4 texcolor = Texel(tex, texture_coords);
     return texcolor * color;
 }
+#endif
