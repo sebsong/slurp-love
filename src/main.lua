af37b751169359e4717e49a2b0e45e5cc12acf32
Sebastian Song <sebastianbsong@gmail.com>
2026-04-07 13:30:23 -0400

    cleanup main file

--- src/main.lua
+++ src/main.lua
@@ -1,15 +1,5 @@
-require("engine/settings")
-require("engine/color")
-require("engine/camera")
-require("engine/draw_utils")
-local collision = require("engine/collision")
 local scene = require("engine/scene")
 
-local game = require("game/game")
-require("game/boat")
-require("game/package")
-local ui = require("game/ui")
-local music = require("game/music")
 local game = require("game/game")
 
 function love.load()
@@ -18,27 +8,17 @@ function love.load()
 	scene.start(game)
 end
 
--- function love.keypressed(key, scancode, isRepeat)
--- 	if key == "space" and not isRepeat then
--- 		if not Boat:pickupPackages(Packages) then
--- 			Boat:deliverPackage(Mailboxes)
--- 		end
--- 	end
+function love.keypressed(key, scancode, isRepeat)
+end
 
--- 	Camera:keypressed(key, scancode, isRepeat)
--- end
+function love.mousepressed(x, y, button, isTouch, presses)
+end
 
--- function love.mousepressed(x, y, button, isTouch, presses)
--- 	Camera:mousepressed(x, y, button, isTouch, presses)
--- end
+function love.mousemoved(x, y, dx, dy, isTouch)
+end
 
--- function love.mousemoved(x, y, dx, dy, isTouch)
--- 	Camera:mousemoved(x, y, dx, dy, isTouch)
--- end
-
--- function love.wheelmoved(x, y)
--- 	Camera:wheelmoved(x, y)
--- end
+function love.wheelmoved(x, y)
+end
 
 function love.update(dt)
 	scene.update(dt)
