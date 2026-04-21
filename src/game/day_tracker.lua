de8f70df69981286fe1578edb3c4df20fde72393
Sebastian Song <seby.song@gmail.com>
2026-04-21 09:58:40 -0400

    text blink; center text

--- src/game/day_tracker.lua
+++ src/game/day_tracker.lua
@@ -2,11 +2,15 @@ local dayTracker = {
 	currentDay = 1
 }
 
+local settings = require("engine/settings")
 local scene = require("engine/scene")
 local font = require("game/font")
 
 local dayTransitionBackgroundImage
-local blinkTimer
+
+local showContinueText = true
+local blinkTimer = 0
+local BLINK_HOLD_TIME = 1
 
 function dayTracker.nextDay()
 	dayTracker.currentDay = dayTracker.currentDay + 1
@@ -34,13 +38,34 @@ function dayTracker.wheelmoved(x, y)
 end
 
 function dayTracker.update(dt)
+	blinkTimer = blinkTimer + dt
+	if blinkTimer > BLINK_HOLD_TIME then
+		blinkTimer = 0
+		showContinueText = not showContinueText
+	end
 end
 
 function dayTracker.draw()
 	love.graphics.setFont(font.default)
 	love.graphics.draw(dayTransitionBackgroundImage)
-	love.graphics.print(string.format("day %s", dayTracker.currentDay), 300, 100)
-	love.graphics.print(string.format("press any button to continue", dayTracker.currentDay), 0, 200)
+	love.graphics.printf(
+		string.format("day %s", dayTracker.currentDay),
+		0,
+		2 * font.default:getHeight(),
+		settings.canvasPixelWidth,
+		"center"
+	)
+
+	if showContinueText then
+		love.graphics.setFont(font.small)
+		love.graphics.printf(
+			string.format("press any button to continue", dayTracker.currentDay),
+			0,
+			settings.canvasPixelHeight - (4 * font.small:getHeight()),
+			settings.canvasPixelWidth,
+			"center"
+		)
+	end
 end
 
 return dayTracker
--- src/game/font.lua
+++ src/game/font.lua
@@ -2,8 +2,11 @@ local font = {
 	default = nil
 }
 
+local CHAR_LAYOUT = " abcdefghijklmnopqrstuvwxyz0123456789"
+
 function font.load()
-	font.default = love.graphics.newImageFont("assets/art/font.png", " abcdefghijklmnopqrstuvwxyz0123456789")
+	font.default = love.graphics.newImageFont("assets/art/font.png", CHAR_LAYOUT)
+	font.small = love.graphics.newImageFont("assets/art/font_small.png", CHAR_LAYOUT)
 end
 
 return font
