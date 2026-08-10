01b0d2dfe433b7b2bff8abc10b6d7adf5fa901a9
Sebastian Song <seby.song@gmail.com>
2026-08-10 00:24:50 -0400

    fix package detail; fix game over; fix victory menu

--- src/game/package.lua
+++ src/game/package.lua
@@ -1,18 +1,11 @@
-local Package = {
-    type = {
-        GLASS = 1,
-        LEAD_FOOT = 2,
-        LANTERN = 3,
-        RADIOACTIVE_JUNK = 4,
-        MIRROR = 5,
-    },
-}
+local Package = {}
 local meta = {}
 meta.__index = meta
 
 local Scene = require("engine.scene")
 
 local PackageDetail = require("game.package_detail")
+local Values = require("game.values")
 
 local crack1Sound
 local crack2Sound
@@ -34,15 +27,15 @@ function meta:onPickup(boat)
         Scene.resume(Scene.scenes.game)
     end)
 
-    if tileId == Package.type.GLASS then
+    if tileId == Values.PACKAGE_TYPES.GLASS then
         self.cracksRemaining = 3
-    elseif tileId == Package.type.LEAD_FOOT then
+    elseif tileId == Values.PACKAGE_TYPES.LEAD_FOOT then
         boat.autoAccelerate = true
-    elseif tileId == Package.type.LANTERN then
+    elseif tileId == Values.PACKAGE_TYPES.LANTERN then
         boat.isLanternActive = true
-    elseif tileId == Package.type.RADIOACTIVE_JUNK then
+    elseif tileId == Values.PACKAGE_TYPES.RADIOACTIVE_JUNK then
         boat.maxSpeed = boat.maxSpeed * 2
-    elseif tileId == Package.type.MIRROR then
+    elseif tileId == Values.PACKAGE_TYPES.MIRROR then
         reversePackageOrder(boat)
     end
 end
@@ -50,20 +43,20 @@ end
 function meta:onDeliver(boat)
     local tileId = self.tileId
 
-    if tileId == Package.type.GLASS then
-    elseif tileId == Package.type.LEAD_FOOT then
+    if tileId == Values.PACKAGE_TYPES.GLASS then
+    elseif tileId == Values.PACKAGE_TYPES.LEAD_FOOT then
         boat.autoAccelerate = false
-    elseif tileId == Package.type.LANTERN then
+    elseif tileId == Values.PACKAGE_TYPES.LANTERN then
         boat.isLanternActive = false
-    elseif tileId == Package.type.RADIOACTIVE_JUNK then
+    elseif tileId == Values.PACKAGE_TYPES.RADIOACTIVE_JUNK then
         boat.maxSpeed = boat.maxSpeed / 2
-    elseif tileId == Package.type.MIRROR then
+    elseif tileId == Values.PACKAGE_TYPES.MIRROR then
         reversePackageOrder(boat)
     end
 end
 
 function meta:onCollision(boat, _collidable)
-    if self.tileId == Package.type.GLASS then
+    if self.tileId == Values.PACKAGE_TYPES.GLASS then
         if not self.canDeliver then
             return
         end
