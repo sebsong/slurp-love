858c57847d15bb98d8d6ce14ea7c67042f5c5520
Sebastian Song <seby.song@gmail.com>
2026-08-11 09:52:32 -0400

    use annotations for camera module

--- src/engine/camera.lua
+++ src/engine/camera.lua
@@ -1,8 +1,32 @@
-local Camera = {}
-local meta = {}
-meta.__index = meta
-
 local Settings = require("engine.settings")
+
+---@class Camera
+---@field transform love.Transform
+---@field screenWidth number
+---@field screenHeight number
+---@field isPanning boolean
+---@field panSpeed number
+---@field zoom number
+---@field zoomToggleIdx number
+---@field zoomToggles number[]
+---@field zoomSpeed number
+---@
+---@field new fun(): Camera
+---@
+---@field getScreenWidth fun(self: Camera): number
+---@field getScreenHeight fun(self: Camera): number
+---@field toggleZoom fun(self: Camera)
+---@field resetZoom fun(self: Camera)
+---@field togglePan fun(self: Camera)
+---@field keyPressed fun(self: Camera, key: love.KeyConstant, scancode: love.Scancode, isRepeat: boolean)
+---@field mousepressed fun(self: Camera, x: number, y: number, button: number, isTouch: boolean, presses: number)
+---@field mousemoved fun(self: Camera, x: number, y: number, dx: number, dy: number, isTouch: boolean)
+---@field wheelmoved fun(self: Camera, x: number, y: number)
+---@field update fun(self: Camera, dt: number)
+---@field getWorldToCanvasTransform fun(self: Camera): love.Transform
+---@field getCanvasToWorldTransform fun(self: Camera): love.Transform
+local Camera = {}
+Camera.__index = Camera
 
 function Camera.new()
     local zoomToggles = { 1, 0.5 }
@@ -18,29 +42,29 @@ function Camera.new()
         zoomToggles = zoomToggles,
         zoomSpeed = 1.1,
     }
-    setmetatable(newCamera, meta)
+    setmetatable(newCamera, Camera)
 
     return newCamera
 end
 
-function meta.getScreenWidth(self)
+function Camera:getScreenWidth()
     return self.screenWidth / self.zoom
 end
 
-function meta.getScreenHeight(self)
+function Camera:getScreenHeight()
     return self.screenHeight / self.zoom
 end
 
-function meta.toggleZoom(self)
+function Camera:toggleZoom()
     self.zoomToggleIdx = math.max((self.zoomToggleIdx + 1) % (#self.zoomToggles + 1), 1)
     self.zoom = self.zoomToggles[self.zoomToggleIdx]
 end
 
-function meta.resetZoom(self)
+function Camera:resetZoom()
     self.zoom = self.zoomToggles[self.zoomToggleIdx]
 end
 
-function meta.togglePan(self)
+function Camera:togglePan()
     self.isPanning = not self.isPanning
     love.mouse.setRelativeMode(self.isPanning)
 
@@ -49,7 +73,7 @@ function meta.togglePan(self)
     end
 end
 
-function meta.keypressed(self, key, scancode, isRepeat)
+function Camera:keypressed(key, scancode, isRepeat)
     if key == "return" and not isRepeat then
         self:toggleZoom()
     end
@@ -59,19 +83,19 @@ function meta.keypressed(self, key, scancode, isRepeat)
     end
 end
 
-function meta.mousepressed(self, x, y, button, isTouch, presses)
+function Camera:mousepressed(x, y, button, isTouch, presses)
     if button == 3 then
         self:togglePan()
     end
 end
 
-function meta.mousemoved(self, x, y, dx, dy, isTouch)
+function Camera:mousemoved(x, y, dx, dy, isTouch)
     if self.isPanning then
         self.transform:translate(dx * self.panSpeed, dy * self.panSpeed)
     end
 end
 
-function meta.wheelmoved(self, x, y)
+function Camera:wheelmoved(x, y)
     if self.isPanning and y ~= 0 then
         local cameraZoomMultiplier = self.zoomSpeed
         if y < 0 then
@@ -81,15 +105,15 @@ function meta.wheelmoved(self, x, y)
     end
 end
 
-function meta.update(self, dt) end
+function Camera:update(dt) end
 
-function Camera.getWorldToCanvasTransform(camera)
-    local camX, camY = camera.transform:transformPoint(0, 0)
-    return love.math.newTransform(-camX + (camera:getScreenWidth() / 2), -camY + (camera:getScreenHeight() / 2))
+function Camera:getWorldToCanvasTransform()
+    local camX, camY = self.transform:transformPoint(0, 0)
+    return love.math.newTransform(-camX + (self:getScreenWidth() / 2), -camY + (self:getScreenHeight() / 2))
 end
 
-function Camera.getCanvasToWorldTransform(camera)
-    return Camera.getWorldToCanvasTransform(camera):inverse()
+function Camera:getCanvasToWorldTransform()
+    self:getWorldToCanvasTransform():inverse()
 end
 
 return Camera
